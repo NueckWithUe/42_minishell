ab0161029c7be79a369db89e80a957c93c948b4f
Nils Nagel <nnagel@student.42heilbronn.de>
2024-07-03 13:07:56 +0200

    feat: added function for freeing the tokens created by the lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,11 +6,24 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 10:02:37 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/03 12:41:24 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/03 13:06:29 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static void	free_tokens(char **tokens)
+{
+	int	i;
+
+	i = 0;
+	while (ft_strncmp(tokens[i], "EOFToken", 8) != 0)
+	{
+		free(tokens[i]);
+		i++;
+	}
+	free(tokens);
+}
 
 static char	**ft_tokenize(char *input, char delimiter)
 {
@@ -41,4 +54,5 @@ void	lexer(char *input)
 		return ;
 	tokens = ft_tokenize(input, ' ');
 	parser(tokens);
+	free_tokens(tokens);
 }
