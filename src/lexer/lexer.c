d3313f80c9e91cb0fd71240695e084c45276edd9
Nils Nagel <nnagel@student.42heilbronn.de>
2024-07-03 12:42:48 +0200

    fix: changed arguments of parser to use the tokens

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,23 +6,11 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 10:02:37 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/03 12:25:02 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/03 12:41:24 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-static void	print_list(char **arr)
-{
-	int	i;
-
-	i = 0;
-	while (arr[i])
-	{
-		ft_printf("Token Nr. %d: [%s]\n", i, arr[i]);
-		i++;
-	}
-}
 
 static char	**ft_tokenize(char *input, char delimiter)
 {
@@ -52,6 +40,5 @@ void	lexer(char *input)
 	if (input[0] == '\0')
 		return ;
 	tokens = ft_tokenize(input, ' ');
-	print_list(tokens);
-	parser();
+	parser(tokens);
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,13 +6,25 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 09:02:21 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/03 10:21:30 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/03 12:41:32 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	parser(void)
+static void	print_list(char **arr)
 {
-	ft_printf("Parser function here\n");
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		ft_printf("Token Nr. %d: [%s]\n", i, arr[i]);
+		i++;
+	}
+}
+
+void	parser(char **tokens)
+{
+	print_list(tokens);
 }
