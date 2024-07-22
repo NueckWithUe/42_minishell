39cc5bf8753a3023e8fbb53271e47f7bdaca9ff4
Nils Nagel <nnagel@student.42heilbronn.de>
2024-07-22 11:11:02 +0200

    feat: implemented logic for 'echo', 'pwd', 'unset' and 'cd'

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:01:00 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/19 11:18:21 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/22 11:08:25 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static void	free_tokens(char **tokens)
 	tokens = NULL;
 }
 
-static void	print_arr(char **arr)
+static int	print_arr(char **arr)
 {
 	int	i;
 
@@ -37,43 +37,25 @@ static void	print_arr(char **arr)
 		ft_printf("%s\n", arr[i]);
 		i++;
 	}
+	return (0);
 }
 
-static char	**sanitize_tokens(char **tokens)
+static int	print_echo(char **tokens, int start)
 {
-	char	**sanitized;
-	int		i;
+	int	i;
 
-	i = 0;
-	sanitized = (char **)malloc(sizeof(char *) * array_size(tokens));
+	i = start;
 	while (!ft_strcomp(tokens[i], "EOFToken"))
 	{
-		sanitized[i] = ft_strdup(tokens[i]);
+		if (ft_strcomp(tokens[i + 1], "EOFToken"))
+			ft_printf("%s", tokens[i]);
+		else
+			ft_printf("%s ", tokens[i]);
 		i++;
 	}
-	sanitized[i] = NULL;
-	return (sanitized);
-}
-
-static void	print_tokens(char **tokens, int m, int i)
-{
-	if (m == 0)
-	{
-		while (tokens[i] != NULL)
-		{
-			ft_printf("%s", tokens[i]);
-			i++;
-		}
-	}
-	else
-	{
-		while (tokens[i] != NULL)
-		{
-			ft_printf("%s ", tokens[i]);
-			i++;
-		}
-		ft_printf("\n");
-	}
+	if (start == 1)
+		write(1, "\n", 1);
+	return (0);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -81,7 +63,6 @@ int	main(int argc, char **argv, char **envp)
 	int		ret;
 	char	*input;
 	char	**tokens;
-	char	**saned_tokens;
 	int		stop = 0;
 
 	ret = 0;
@@ -93,41 +74,29 @@ int	main(int argc, char **argv, char **envp)
 	{
 		input = readline(">> ");
 		tokens = lexer(input);
-		saned_tokens = sanitize_tokens(tokens);
 		// parser(tokens); //check tokens for grammar
 		if (ft_strcomp(input, "stop"))
-			break ;
-		else if (ft_strcomp(input, "$?"))
-		{
-			ft_printf("%d\n", ret);
-			ret = 0;
-		}
-		if (ft_strchr(input, '|'))
-			handle_pipe(tokens);
+			stop = 1;
+		// if (ft_strchr(input, '|'))
+		// 	handle_pipe(tokens);
 		if (ft_strcomp(tokens[0], "echo"))
 		{
 			if (ft_strcomp(tokens[1], "-n"))
-			{
-				print_tokens(saned_tokens, 0, 2);
-				ret = 0;
-			}
+				ret = print_echo(tokens, 2);
 			else
-			{
-				print_tokens(saned_tokens, 1, 1);
-				ret = 0;
-			}
+				ret = print_echo(tokens, 1);
 		}
 		if (ft_strcomp(tokens[0], "env") && ft_strcomp(tokens[1], "EOFToken"))
-		{
-			print_arr(envp);
-			ret = 0;
-		}
+			ret = print_arr(envp);
+		else if (ft_strcomp(tokens[0], "pwd") && ft_strcomp(tokens[1], "EOFToken"))
+			ft_printf("%s\n", getenv("PWD"));
 		else if (ft_strcomp(tokens[0], "exit") && ft_strcomp(tokens[1], "EOFToken"))
 			exit(ret);
 		else if (ft_strcomp(tokens[0], "cd"))
 			ret = cd(tokens[1]);
+		else if (ft_strcomp(tokens[0], "unset") && ft_strcomp(tokens[1], "EOFToken"))
+			ret = 0;
 		free_tokens(tokens);
-		free_tokens(saned_tokens);
 	}
 	return (ret);
 }
