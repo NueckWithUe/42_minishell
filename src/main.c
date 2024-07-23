2b815bf71867cedfcd06c834baa71f0cc188fe31
Nils Nagel <nnagel@student.42heilbronn.de>
2024-07-23 02:37:55 +0200

    fix: exit can now use an exit status

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:01:00 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/22 23:36:11 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/23 02:32:29 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,20 @@ static void	free_tokens(char **tokens)
 	}
 	free(tokens);
 	tokens = NULL;
+}
+
+static int	str_is_num(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -55,8 +69,13 @@ int	main(int argc, char **argv, char **envp)
 			ret = print_arr(envp);
 		else if (ft_strcomp(tokens[0], "pwd") && ft_strcomp(tokens[1], "EOFToken"))
 			ft_printf("%s\n", getenv("PWD"));
-		else if (ft_strcomp(tokens[0], "exit") && ft_strcomp(tokens[1], "EOFToken"))
-			exit(ret);
+		else if (ft_strcomp(tokens[0], "exit"))
+		{
+			if (ft_strcomp(tokens[1], "EOFToken"))
+				exit(ret);
+			else if (str_is_num(tokens[1]))
+				exit(ft_atoi(tokens[1]));
+		}
 		else if (ft_strcomp(tokens[0], "cd"))
 			ret = cd(envp, tokens[1]);
 		else if (ft_strcomp(tokens[0], "unset"))
