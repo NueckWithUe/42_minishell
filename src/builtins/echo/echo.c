860b8ef941f2a243aba9ee80788cd1c5abd5f963
Nils Nagel <nnagel@student.42heilbronn.de>
2024-07-30 18:03:36 +0200

    fix: echo can now print the latest return value

--- src/builtins/echo/echo.c
+++ src/builtins/echo/echo.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 20:54:36 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/30 17:08:23 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/30 18:02:32 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,15 @@ int	print_echo(char **tokens, int start)
 	i = start;
 	while (!ft_strcomp(tokens[i], "EOFToken"))
 	{
+		if (ft_strcomp(tokens[i], "$?"))
+		{
+			ft_printf("latest return value"); //print return value; will be stored in semi global struct t_data
+			i++;
+			if (ft_strcomp(tokens[i], "EOFToken"))
+				break ;
+			else
+				write(1, " ", 1);
+		}
 		convert = convert_env(tokens[i]);
 		if (ft_strcomp(tokens[i + 1], "EOFToken"))
 			ft_printf("%s", convert);
