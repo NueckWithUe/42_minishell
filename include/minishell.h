66ffb17d9262526a47049a070bd46cc6e32584b3
Nils Nagel <nnagel@student.42heilbronn.de>
2024-07-30 16:44:30 +0200

    fix: environment variables are now in a linked list, adjusted rest of the program to it

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:01:09 by nnagel            #+#    #+#             */
-/*   Updated: 2024/07/25 10:16:57 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/07/30 16:34:50 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,13 @@
 
 # include "libft.h"
 
+typedef struct s_env
+{
+	struct s_env	*next;
+	struct s_env	*prev;
+	void			*data;
+}	t_env;
+
 char	**parser(char **tokens);
 char	**lexer(char *input);
 
@@ -40,24 +47,30 @@ int		ft_get_wc(char *str, char delimiter);
 char	*ft_strtok(char *str, char delimiter);
 
 /* pipes */
-void	handle_pipe(char **envp, char **tokens);
+void	handle_pipe(t_env *envp, char **tokens);
 void	get_command(char **tokens, char ***com1, char ***com2);
 void	check_pid(pid_t pid);
 
 /* cd */
-int		cd(char **envp, char *path);
+int		cd(t_env *envp, char *path);
 
 /* echo */
 int		print_echo(char **tokens, int start);
 
 /* env */
-int		print_arr(char **arr);
+void	print_list(t_env *lst);
+void	search_and_replace(t_env **start, char *to_replace);
+void	search_and_remove(t_env **start, char *to_remove);
 
 /* export */
-int		export(char **envp, char **tokens);
+// int		export(char **envp, char **tokens);
+int		export(t_env *envp, char **tokens);
+void	add_exports(t_env *envp, char **tokens);
+t_env	*convert_env_to_list(char **envp);
+void	ft_insert_at_end(t_env **start, char *data);
 
 /* unset */
-int		unset(char ***envp, char **tokens);
+int		unset(t_env *envp, char **tokens);
 
 /* general utils */
 int		ft_strcomp(char *str1, char *str2);
