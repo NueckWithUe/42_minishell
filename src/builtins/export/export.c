7ab9b9850f3f26aa9f732d3962ab5a49acf8a8c4
Nueck <nils1.nagel9@gmail.com>
2024-07-30 08:26:54 +0200

    fix: working on sorting array

--- src/builtins/export/export.c
+++ src/builtins/export/export.c
@@ -16,16 +16,16 @@ static char **sort_ascend(char **envp)
 {
 	char	**sorted;
 	int		i;
-	int		j;
 
 	i = 0;
-	j = 0;
 	sorted = malloc(sizeof(char *) * array_size(envp) + 1);
 	while (envp[i])
 	{
+		sorted[i] = ft_strdup(envp[i]);
 		i++;
 	}
 	return (sorted);
+	//go through string and search for the lowest value. Then search for the lowest value in relation to the current value
 }
 
 static void	print_exports(char **envp)
@@ -37,7 +37,7 @@ static void	print_exports(char **envp)
 	i = 0;
 	while (sorted[i])
 	{
-		ft_printf("declare -x %s\n", sorted[i]);
+		ft_printf("declare\t-x %s\n", sorted[i]);
 		i++;
 	}
 }
@@ -53,76 +53,3 @@ int	export(char **envp, char **tokens)
 		i++;
 	return (0);
 }
-
-// static char *get_first_line(char **arr)
-// {
-// 	int i;
-// 	int index_smallest;
-
-// 	i = 1;
-// 	index_smallest = 0;
-// 	while (arr[i])
-// 	{
-// 		// if (ft_strncmp(arr[i], arr[index_smallest], sizeof(long)));
-
-// 		i++;
-// 	}
-// 	return (arr[index_smallest]);
-// }
-
-// static void sort_ascend(char **arr, char ***sorted)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	j = 0;
-// 	if (*sorted == NULL)
-// 		*sorted = malloc(sizeof(char *) * size_array(arr) + 1);
-// 	while (arr[i])
-// 	{
-// 		(*sorted)[j] = ft_strdup(arr[i]);
-// 		j++;
-// 		i++;
-// 	}
-// 	if (size_array(*sorted) != size_array(arr))
-// 		sort_ascend(arr, sorted);
-// }
-
-// static void compare_strings(char **arr)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	j = 0;
-// 	while (arr[i])
-// 	{
-// 		while (arr[j])
-// 		{
-// 			if (i == j)
-// 				j++;
-// 			printf("%c compared with %c: %d\n", arr[i][0], arr[j][0], ft_strncmp(arr[i], arr[j], sizeof(long)));
-// 			j++;
-// 		}
-// 		i++;
-// 		j = 0;
-// 	}
-// }
-
-// int main()
-// {
-// 	char **arr;
-// 	char **sorted;
-
-// 	sorted = NULL;
-// 	arr = malloc(sizeof(char *) * 3);
-// 	arr[0] = ft_strdup("Test");
-// 	arr[1] = ft_strdup("Sorting");
-// 	arr[2] = ft_strdup("Arrays");
-// 	compare_strings(arr);
-// 	sort_ascend(arr, &sorted);
-// 	print_array(arr);
-// 	write(1, "\n", 1);
-// 	print_array(sorted);
-// }
